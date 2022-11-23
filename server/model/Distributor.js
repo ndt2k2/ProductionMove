0eabf108c21347df0f552a5cebe9766ae903cd89
ndt2k2 <ngduytann@gmail.com>
2022-11-23 22:44:32 +0700

    2

--- server/model/Distributor.js
+++ server/model/Distributor.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const DistributorSchema = new mongoose.Schema({
     ID: {
-        
+        type: String,
+        require: true,
+    },
+    name: {
+        type: String,
+        require: true,
     }
 }, { timestamps: true });
 module.exports = mongoose.model("Distributor", DistributorSchema);
