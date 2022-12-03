7ccb9819e79eb983ecfe466c3044e92a50efcc87
ndt2k2 <ngduytann@gmail.com>
2022-12-03 23:17:56 +0700

    1

---
+++ server/model/ErBackFactory.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const ErBackFactorySchema = new mongoose.Schema({
+    ID: {
+        type: String,
+        required: true,
+    },
+    error: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        required: true,
+    },
+    idDistributor: {
+        type: String,
+        required: true,
+    },
+    idServiceCenter: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+module.exports = mongoose.model("ErBackFactory", ErBackFactorySchema);
---
+++ server/model/Recall.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const RecallSchema = new mongoose.Schema({
+    ID: {
+        type: String,
+        required: true,
+        unique: true,
+d    },
+    status: {
+        type: String,
+        required: true,
+    },
+    error: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true });
+module.exports = mongoose.model("Recall", RecallSchema);
