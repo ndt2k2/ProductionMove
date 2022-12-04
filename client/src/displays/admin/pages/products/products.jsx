541a3bec3829f70a725443d2fed2ca178284a637
Quyet <20020467@vnu.edu.vn>
2022-12-04 22:59:12 +0700

    update

--- client/src/displays/admin/pages/products/products.jsx
+++ client/src/displays/admin/pages/products/products.jsx
@@ -5,58 +5,142 @@ import Product from '../../../../components/product/product';
 import UserActions from '../../../../components/userActions/userActions';
 
 import {Box} from '@mui/material'
-import {DataGrid } from '@mui/x-data-grid'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import CancelIcon from '@mui/icons-material/Close';
+import {DataGrid, GridActionsCellItem, GridRowModes } from '@mui/x-data-grid'
 
-import { useState } from 'react';
+
+import React from 'react';
+import { useState, useMemo } from 'react';
+
 
 export default function Products() {
     const [pageSize, setPageSize] = useState(10);
-    const [selectionModel, setSelectionModel] = useState([]);
-    console.log(selectionModel);
 
     const [rows, setRows] = useState([
-        { id: '1', name: "Raj", email: "Raj@gmail.com", phone: 7894561230, age: null, gender: "M", city: "Chennai", fee: 78456 },
-        { id: '2',name: "Mohan", email: "mohan@gmail.com", phone: 7845621590, age: 35, gender: "M", city: "Delhi", fee: 456125 },
-        { id: '3',name: "Sweety", email: "sweety@gmail.com", phone: 741852912, age: 17, gender: "F", city: "Noida", fee: 458796 },
-        { id: '4',name: "Vikas", email: "vikas@gmail.com", phone: 9876543210, age: 20, gender: "M", city: "Mumbai", fee: 874569 },
-        { id: '5',name: "Neha", email: "neha@gmail.com", phone: 7845621301, age: 25, gender: "F", city: "Patna", fee: 748521 },
-        { id: '6',name: "Mohan", email: "mohan@gmail.com", phone: 7845621590, age: 35, gender: "M", city: "Delhi", fee: 456125 },
-        { id: '7',name: "Sweety", email: "sweety@gmail.com", phone: 741852912, age: 17, gender: "F", city: "Noida", fee: 458796 },
-        { id: '8',name: "Vikas", email: "vikas@gmail.com", phone: 9876543210, age: 20, gender: "M", city: "Mumbai", fee: 874569 },
-        { id: '9',name: "Raj", email: "Raj@gmail.com", phone: 7894561230, age: null, gender: "M", city: "Chennai", fee: 78456 },
-        { id: '10',name: "Mohan", email: "mohan@gmail.com", phone: 7845621590, age: 35, gender: "M", city: "Delhi", fee: 456125 },
-        { id: '11',name: "Sweety", email: "sweety@gmail.com", phone: 741852912, age: 17, gender: "F", city: "Noida", fee: 458796 },
-        { id: '12',name: "Vikas", email: "vikas@gmail.com", phone: 9876543210, age: 20, gender: "M", city: "Mumbai", fee: 874569 },
+        { id: 1, name: "Raj", email: "Raj@gmail.com", phone: 7894561230, age: null, gender: "M", city: "Chennai", fee: 78456 },
+        { id: 2,name: "Mohan", email: "mohan@gmail.com", phone: 7845621590, age: 35, gender: "M", city: "Delhi", fee: 456125 },
+        { id: 3,name: "Sweety", email: "sweety@gmail.com", phone: 741852912, age: 17, gender: "F", city: "Noida", fee: 458796 },
+        { id: 4,name: "Vikas", email: "vikas@gmail.com", phone: 9876543210, age: 20, gender: "M", city: "Mumbai", fee: 874569 },
+        { id: 5,name: "Neha", email: "neha@gmail.com", phone: 7845621301, age: 25, gender: "F", city: "Patna", fee: 748521 },
+        { id: 6,name: "Mohan", email: "mohan@gmail.com", phone: 7845621590, age: 35, gender: "M", city: "Delhi", fee: 456125 },
+        { id: 7,name: "Sweety", email: "sweety@gmail.com", phone: 741852912, age: 17, gender: "F", city: "Noida", fee: 458796 },
+        { id: 8,name: "Vikas", email: "vikas@gmail.com", phone: 9876543210, age: 20, gender: "M", city: "Mumbai", fee: 874569 },
+        { id: 9,name: "Raj", email: "Raj@gmail.com", phone: 7894561230, age: null, gender: "M", city: "Chennai", fee: 78456 },
+        { id: 10,name: "Mohan", email: "mohan@gmail.com", phone: 7845621590, age: 35, gender: "M", city: "Delhi", fee: 456125 },
+        { id: 11,name: "Sweety", email: "sweety@gmail.com", phone: 741852912, age: 17, gender: "F", city: "Noida", fee: 458796 },
+        { id: 12,name: "Vikas", email: "vikas@gmail.com", phone: 9876543210, age: 20, gender: "M", city: "Mumbai", fee: 874569 },
       ])
 
     const columns = [
-        { title: "Id", field: "id", width: 90, editable: true },
-        { title: "Name", field: "name" , width: 120, editable: true },
-        { title: "Email", field: "email", width: 200, editable: true  },
-        { title: "Phone Number", field: "phone", width: 120, editable: true },
-        { title: "Age", field: "age", width: 90, editable: true },
-        { title: "Gender", field: "gender", width: 120, editable: true },
-        { title: "City", field: "city", width: 120, editable: true },
-        { title: "School Fee", field: "fee", width: 90, editable: true  },
-        { title:"actions", field: "actions", type: "actions", renderCell:params=> <UserActions {...{params}}/>}
+            { title: "Id", field: "id", width: 90, editable: true },
+            { title: "Name", field: "name" , width: 120, editable: true },
+            { title: "Email", field: "email", width: 200, editable: true  },
+            { title: "Phone Number", field: "phone", width: 120, editable: true },
+            { title: "Age", field: "age", width: 90, editable: true },
+            { title: "Gender", field: "gender", width: 120, editable: true },
+            { title: "City", field: "city", width: 120, editable: true },
+            { title: "School Fee", field: "fee", width: 90, editable: true  },
+            {
+                field: 'actions',
+                type: 'actions',
+                headerName: 'Actions',
+                width: 100,
+                cellClassName: 'actions',
+                getActions: ({ id }) => {
+                  const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+          
+                  if (isInEditMode) {
+                    return [
+                      <GridActionsCellItem
+                        icon={<SaveIcon />}
+                        label="Save"
+                        onClick={handleSaveClick(id)}
+                      />,
+                      <GridActionsCellItem
+                        icon={<CancelIcon />}
+                        label="Cancel"
+                        className="textPrimary"
+                        onClick={handleCancelClick(id)}
+                        color="inherit"
+                      />,
+                    ];
+                  }
+          
+                  return [
+                    <GridActionsCellItem
+                      icon={<EditIcon />}
+                      label="Edit"
+                      className="textPrimary"
+                      onClick={handleEditClick(id)}
+                      color="inherit"
+                    />,
+                    <GridActionsCellItem
+                      icon={<DeleteIcon />}
+                      label="Delete"
+                      onClick={handleDeleteClick(id)}
+                      color="inherit"
+                    />,
+                  ];
+                },
+              },
     ]
 
-    const handleDelete = () => {
-        setSelectionModel([])
+ 
+
+    const [rowModesModel, setRowModesModel] = React.useState({});
+
+    const handleRowEditStart = (params, event) => {
+        event.defaultMuiPrevented = true;
     };
+
+    const handleRowEditStop = (params, event) => {
+        event.defaultMuiPrevented = true;
+    };
+
+    const handleEditClick = (id) => () => {
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+    };
+
+    const handleSaveClick = (id) => () => {
+        console.log(rowModesModel);
+        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
+    };
+
+    const handleDeleteClick = (id) => () => {
+        console.log(id);
+        setRows(rows.filter((row) => row.id !== id));
+    };
+
+    const handleCancelClick = (id) => () => {
+        setRowModesModel({
+        ...rowModesModel,
+        [id]: { mode: GridRowModes.View, ignoreModifications: true },
+        });
+
+        const editedRow = rows.find((row) => row.id === id);
+        if (editedRow.isNew) {
+        setRows(rows.filter((row) => row.id !== id));
+        }
+    };
+
+    const processRowUpdate = (newRow) => {
+        console.log(newRow);
+        const updatedRow = { ...newRow, isNew: false };
+        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
+        return updatedRow;
+    };
+
+
 
     return(
         <div className="products">
             <Sidebar/>
             <div className="wrapper">
                 <Navbar/>
-                <button 
-                    style={{width:100}}
-                    onClick = {()=>handleDelete()}
-                >
-                    delete products
-                </button>
-
+                
                 <div className="mainProduct">
                        <Box 
                          sx={{
@@ -68,23 +152,27 @@ export default function Products() {
                           }}
                         >
                             <DataGrid
-                            columns={columns}
-                            rows={rows}
-                            getRowId={rows.id}
-                            checkboxSelection
-                            editMode="row"
-                            disableSelectionOnClick
-                            pagination
-                            pageSize={pageSize}
-                            onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-                            rowsPerPageOptions={[10, 20, 40]}
+                                columns={columns}
+                                rows={rows}
+                                getRowId={rows.id}
+                                disableSelectionOnClick
 
-                            onSelectionModelChange={(newSelectionModel) => {
-                                setSelectionModel(newSelectionModel);
-                              }}
-                            selectionModel={selectionModel}
+                                pagination
+                                pageSize={pageSize}
+                                onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+                                rowsPerPageOptions={[10, 20, 40]}
+
+                                editMode="row"
+                                rowModesModel={rowModesModel}
+                                onRowModesModelChange={(newModel) => setRowModesModel(newModel)}
+                                onRowEditStart={handleRowEditStart}
+                                onRowEditStop={handleRowEditStop}
+                                processRowUpdate={processRowUpdate}
+                               
+                                experimentalFeatures={{ newEditingApi: true }}
                             />
                        </Box>
+                    
                 </div>
             </div>
         </div>
