495ed5b901876a4bd1972cf9f8c64d630a5fdc6d
Quyet <20020467@vnu.edu.vn>
2022-12-25 21:19:54 +0700

    update chuc nang

--- client/src/displays/admin/pages/products/products.jsx
+++ client/src/displays/admin/pages/products/products.jsx
@@ -9,23 +9,39 @@ import SaveIcon from "@mui/icons-material/Save";
 import CancelIcon from "@mui/icons-material/Close";
 import { GridActionsCellItem, GridRowModes } from "@mui/x-data-grid";
 
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 export default function Products() {
   const height = 631;
-
   const [rowModesModel, setRowModesModel] = React.useState({});
+
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    const getAllProduct = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000/api/product/getAll");
+        setRows(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllProduct();
+  }, []);
+
+  console.log("Rerender");
 
   const handleEditClick = (id) => () => {
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
   };
 
   const handleSaveClick = (id) => () => {
-    console.log(rowModesModel);
+    // console.log(rowModesModel);
     setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-    console.log(rowModesModel[id]);
+    // console.log(rowModesModel[id]);
   };
 
   const handleDeleteClick = (id) => () => {
@@ -45,138 +61,14 @@ export default function Products() {
     }
   };
 
-  const [rows, setRows] = useState([
-    {
-      id: 1,
-      name: "Raj",
-      email: "Raj@gmail.com",
-      phone: 7894561230,
-      age: null,
-      gender: "M",
-      city: "Chennai",
-      fee: 78456,
-    },
-    {
-      id: 2,
-      name: "Mohan",
-      email: "mohan@gmail.com",
-      phone: 7845621590,
-      age: 35,
-      gender: "M",
-      city: "Delhi",
-      fee: 456125,
-    },
-    {
-      id: 3,
-      name: "Sweety",
-      email: "sweety@gmail.com",
-      phone: 741852912,
-      age: 17,
-      gender: "F",
-      city: "Noida",
-      fee: 458796,
-    },
-    {
-      id: 4,
-      name: "Vikas",
-      email: "vikas@gmail.com",
-      phone: 9876543210,
-      age: 20,
-      gender: "M",
-      city: "Mumbai",
-      fee: 874569,
-    },
-    {
-      id: 5,
-      name: "Neha",
-      email: "neha@gmail.com",
-      phone: 7845621301,
-      age: 25,
-      gender: "F",
-      city: "Patna",
-      fee: 748521,
-    },
-    {
-      id: 6,
-      name: "Mohan",
-      email: "mohan@gmail.com",
-      phone: 7845621590,
-      age: 35,
-      gender: "M",
-      city: "Delhi",
-      fee: 456125,
-    },
-    {
-      id: 7,
-      name: "Sweety",
-      email: "sweety@gmail.com",
-      phone: 741852912,
-      age: 17,
-      gender: "F",
-      city: "Noida",
-      fee: 458796,
-    },
-    {
-      id: 8,
-      name: "Vikas",
-      email: "vikas@gmail.com",
-      phone: 9876543210,
-      age: 20,
-      gender: "M",
-      city: "Mumbai",
-      fee: 874569,
-    },
-    {
-      id: 9,
-      name: "Raj",
-      email: "Raj@gmail.com",
-      phone: 7894561230,
-      age: null,
-      gender: "M",
-      city: "Chennai",
-      fee: 78456,
-    },
-    {
-      id: 10,
-      name: "Mohan",
-      email: "mohan@gmail.com",
-      phone: 7845621590,
-      age: 35,
-      gender: "M",
-      city: "Delhi",
-      fee: 456125,
-    },
-    {
-      id: 11,
-      name: "Sweety",
-      email: "sweety@gmail.com",
-      phone: 741852912,
-      age: 17,
-      gender: "F",
-      city: "Noida",
-      fee: 458796,
-    },
-    {
-      id: 12,
-      name: "Vikas",
-      email: "vikas@gmail.com",
-      phone: 9876543210,
-      age: 20,
-      gender: "M",
-      city: "Mumbai",
-      fee: 874569,
-    },
-  ]);
-
   const columns = [
-    { title: "Id", field: "id", width: 90, editable: true },
+    { title: "id", field: "id", width: 90, editable: false },
     { title: "Name", field: "name", width: 120, editable: true },
-    { title: "Email", field: "email", width: 200, editable: true },
-    { title: "Phone Number", field: "phone", width: 120, editable: true },
-    { title: "Age", field: "age", width: 90, editable: true },
-    { title: "Gender", field: "gender", width: 120, editable: true },
-    { title: "City", field: "city", width: 120, editable: true },
-    { title: "School Fee", field: "fee", width: 90, editable: true },
+    { title: "Color", field: "color", width: 120, editable: true },
+    { title: "Factory", field: "factory", width: 120, editable: true },
+    { title: "Status", field: "status", width: 120, editable: true },
+    { title: "Owner", field: "owner", width: 120, editable: true },
+
     {
       field: "actions",
       type: "actions",
@@ -274,7 +166,6 @@ export default function Products() {
             rows,
             setRows,
             height,
-
             rowModesModel,
             setRowModesModel,
           }}
