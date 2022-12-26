c7332e2c5ab975f8f7f57467677cc1927299e8f4
huyanh <vuhuyanh44@gmail.com>
2022-12-26 20:49:12 +0700

    fix css

--- client/src/displays/signIn/signIn.jsx
+++ client/src/displays/signIn/signIn.jsx
@@ -1,15 +1,61 @@
 import './signIn.css';
-
+import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import { ToastContainer, toast } from "react-toastify";
 import { useContext, useRef,useEffect,useState } from "react";
 import Slider from './slider/slider'
 import {BsFillPersonFill} from 'react-icons/bs'
 import {RiLockPasswordFill} from 'react-icons/ri'
-export default function SignIn() {
+// import { loginRoute } from "src/"
 
-    const username= useRef();
-    const password= useRef();
-  
-    return (
+
+
+export default function SignIn() {
+  // const navigate = useNavigate();
+  // const [values, setValues] = useState({ username: "", password: "" });
+  // const handleChange = (event) => {
+  //   setValues({ ...values, [event.target.name]: event.target.value });
+  // };
+  // const validateForm = () => {
+  //   const { username, password } = values;
+  //   const toastOptions = {
+  //     position: "bottom-right",
+  //     autoClose: 8000,
+  //     pauseOnHover: true,
+  //     draggable: true,
+  //     theme: "dark",
+  //   };
+  //   if (username === "") {
+  //     toast.error("Email and Password is required.", toastOptions);
+  //     return false;
+  //   } else if (password === "") {
+  //     toast.error("Email and Password is required.", toastOptions);
+  //     return false;
+  //   }
+  //   return true;
+  // };
+  // const handleSubmit = async (event) => {
+  //   event.preventDefault();
+  //   if (validateForm()) {
+  //     const { username, password } = values;
+  //     const { data } = await axios.post(loginRoute, {
+  //       username,
+  //       password,
+  //     });
+  //     // if (data.status === false) {
+  //     //   toast.error(data.msg, toastOptions);
+  //     // }
+  //     if (data.status === true) {
+  //       localStorage.setItem(
+  //         process.env.REACT_APP_LOCALHOST_KEY,
+  //         JSON.stringify(data.user)
+  //       );
+
+  //       navigate("/");
+  //     }
+  //   }
+  // };
+  return (
       <div className="signIn">
         <div className="signInWrapper">
           
@@ -24,21 +70,29 @@ export default function SignIn() {
               <div className="signInForm">
                 <div className="signInFormContainer">
   
-                  <form className="signInBox">
+                  <form className="signInBox" 
+                  // onSubmit={(e) => handleSubmit(e)}
+                  >
                     <div>
                     {/* <span className='spanSI'>Username</span> */}
-                    <BsFillPersonFill className="icon"/>
-                    <input placeholder="Username" required className="signInInput" ref={username} autofocus /*onFocus={handleFocus}*/ />
+                    <BsFillPersonFill className="iconSignin"/>
+                    <input placeholder="Username" required 
+                    className="signInInput" 
+                    // ref={username} 
+                    autofocus
+                    // onChange={(e) => handleChange(e)} /*onFocus={handleFocus}*/ 
+                    />
                     </div>
                     <div>
                       {/* <span className='spanSI'>Password</span> */}
-                      <RiLockPasswordFill className='icon'/>
+                      <RiLockPasswordFill className='iconSignin'/>
                       <input
                       placeholder="Password"
                       type="password"
                       required
                       className="signInInput"
-                      ref={password}
+                      // ref={password}
+                      // onChange={(e) => handleChange(e)}
                     //   onFocus={handleFocus}
                     />
                     </div>
@@ -47,11 +101,11 @@ export default function SignIn() {
                       {formWarning}
                     </h4> */}
                     <button className="signInButton" type="submit"
-                    // onClick={handleSubmit}
                     >
                       Sign In
                     </button>
                   </form>
+                  {/* <ToastContainer /> */}
                 </div>
               </div>
               <div className="signInFooter">
